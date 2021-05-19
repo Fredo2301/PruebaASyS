f1af31240fecf576a3673f12a961a532e8d476df
Fredo2301 <84367441+Fredo2301@users.noreply.github.com>
2021-05-19 16:08:21 -0500

    Add files via upload

---
+++ Practica_1_ASyS/Practica1.m
@@ -0,0 +1,18 @@
+%% Practica 1 (P01). Señales en tiempo continuo. 
+%
+%% Integrantes
+% * Aleks Adrián Calderón Vázquez
+% * Carlos Josafath López Favila
+% * Fredy Bautista Dominguez
+% * Irvin Adrián Vázquez Ruíz
+% * Iván Rodrigo Santacruz Bravo
+%% Objetivos
+% * Manipulación básica de MATLAB
+% * Gráficas de señales reales y complejas continuas
+% * Transformación de señales continuas (escalamientos y traslaciones)
+% * Calculo de energía y potencia de señales continuas
+%
+%% Introducción
+%
+%% Desarrollo
+
